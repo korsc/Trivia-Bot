3c5956e71bfca68f494679ce978c96c895742b38
AmmarA <118152219+korsc@users.noreply.github.com>
2023-04-08 00:11:35 -0400

    Add files via upload

---
+++ discordtriviabot/game_questions.py
@@ -0,0 +1,181 @@
+gamequestions = [
+    {
+        "question": "What is the name of Mario's brother?",
+        "options": ["Luigi", "Wario", "Bowser", "Yoshi"],
+        "answer": "Luigi"
+    },
+    {
+        "question": "What is the name of Sonic's sidekick?",
+        "options": ["Tails", "Knuckles", "Shadow", "Amy"],
+        "answer": "Tails"
+    },
+    {
+        "question": "In League of Legends, which champion has the ultimate ability 'Enchanted Crystal Arrow'?",
+        "options": ["Ashe", "Jinx", "Vayne", "Caitlyn"],
+        "answer": "Ashe"
+    },
+    {
+        "question": "What type of Pokemon is Pikachu?",
+        "options": ["Electric", "Water", "Fire", "Grass"],
+        "answer": "Electric"
+    },
+    {
+        "question": "In Minecraft, what is the main material used to build Nether portals?",
+        "options": ["Obsidian", "End Stone", "Netherrack", "Glowstone"],
+        "answer": "Obsidian"
+    },
+    {
+        "question": "What is the name of the Pokemon professor in the Kanto region?",
+        "options": ["Professor Oak", "Professor Elm", "Professor Birch", "Professor Sycamore"],
+        "answer": "Professor Oak"
+    },
+    {
+        "question": "What is the name of the first level in Super Mario Bros.?",
+        "options": ["World 1-1", "Level 1-1", "Stage 1-1", "Area 1-1"],
+        "answer": "World 1-1"
+    },
+    {
+        "question": "Which Pokemon game introduced the Fairy type?",
+        "options": ["X and Y", "Black and White", "Sun and Moon", "Omega Ruby and Alpha Sapphire"],
+        "answer": "X and Y"
+    },
+    {
+        "question": "In League of Legends, which champion has the ultimate ability 'Grand Skyfall'?",
+        "options": ["Pantheon", "Jax", "Garen", "Darius"],
+        "answer": "Pantheon"
+    },
+    {
+        "question": "What type of Pokemon is Charmander?",
+        "options": ["Fire", "Water", "Electric", "Grass"],
+        "answer": "Fire"
+    },
+    {
+        "question": "In Minecraft, what item is required to craft a brewing stand?",
+        "options": ["Blaze Rod", "Ender Pearl", "Nether Wart", "Ghast Tear"],
+        "answer": "Blaze Rod"
+    },
+    {
+        "question": "What is the name of the princess in Super Mario Bros.?",
+        "options": ["Peach", "Daisy", "Rosalina", "Toadstool"],
+        "answer": "Peach"
+    },
+    {
+        "question": "What is the name of the main character in Minecraft?",
+        "options": ["Steve", "Alex", "Notch", "Herobrine"],
+        "answer": "Steve"
+    },
+    {
+        "question": "What type of Pokemon is Charizard?",
+        "options": ["Fire/Flying", "Dragon/Flying", "Fire/Dragon", "Fire/Dark"],
+        "answer": "Fire/Flying"
+    },
+    {
+        "question": "What is the name of the villainous team in Pokemon Red and Blue?",
+        "options": ["Team Rocket", "Team Magma", "Team Aqua", "Team Plasma"],
+        "answer": "Team Rocket"
+    },
+    {
+        "question": "In League of Legends, which champion has the ultimate ability 'Chronoshift'?",
+        "options": ["Zilean", "Soraka", "Kayle", "Morgana"],
+        "answer": "Zilean"
+    },
+    {
+        "question": "What is the name of Sonic's arch-nemesis?",
+        "options": ["Dr. Eggman", "Shadow the Hedgehog", "Metal Sonic", "Knuckles the Echidna"],
+        "answer": "Dr. Eggman"
+    },
+    {
+        "question": "What is the name of Mario's dinosaur companion?",
+        "options": ["Yoshi", "Peach", "Toad", "Bowser Jr."],
+        "answer": "Yoshi"
+    },
+    {
+        "question": "In the Legend of Zelda series, what is the name of the main protagonist?",
+        "options": ["Link", "Zelda", "Ganondorf", "Impa"],
+        "answer": "Link"
+    },
+    {
+        "question": "What is the name of the titular character in the Metroid series?",
+        "options": ["Samus Aran", "Ridley", "Kraid", "Mother Brain"],
+        "answer": "Samus Aran"
+    },
+    {
+        "question": "In Super Smash Bros, which character has a super strong punch special?",
+        "options": ["Captain Falcon", "Fox McCloud", "Donkey Kong", "Kirby"],
+        "answer": "Captain Falcon"
+    },
+    {
+        "question": "What is the name of the protagonist in the Fire Emblem series?",
+        "options": ["Marth", "Lucina", "Roy", "Ike"],
+        "answer": "Marth"
+    },
+    {
+        "question": "In Minecraft, what is the name of the final boss?",
+        "options": ["The Ender Dragon", "The Wither", "Herobrine", "The Elder Guardian"],
+        "answer": "The Ender Dragon"
+    },
+    {
+        "question": "What is the name of the grass-type starter Pokemon in Pokemon Red and Blue?",
+        "options": ["Bulbasaur", "Sceptile", "Snivy", "Turtwig"],
+        "answer": "Bulbasaur"
+    },
+    {
+        "question": "What is the name of the Pokemon professor in Pokemon Red and Blue?",
+        "options": ["Professor Oak", "Professor Elm", "Professor Juniper", "Professor Sycamore"],
+        "answer": "Professor Oak"
+    },
+    {
+        "question": "In League of Legends, which champion has the ultimate ability 'Death Mark'?",
+        "options": ["Zed", "Akali", "Katarina", "LeBlanc"],
+        "answer": "Zed"
+    },
+    {
+        "question": "What is the name of the first Pokemon in the Pokedex?",
+        "options": ["Bulbasaur", "Charmander", "Squirtle", "Pikachu"],
+        "answer": "Bulbasaur"
+    },
+    {
+        "question": "In Minecraft, what is the name of the alternate dimension that can be accessed by creating a portal made of obsidian?",
+        "options": ["The End", "The Void", "The Abyss", "The Nether"],
+        "answer": "The Nether"
+    },
+    {
+        "question": "What is the name of the device that Mario uses to travel between different worlds in Super Mario Bros. 3?",
+        "options": ["Warp Whistle", "Starman", "Magic Mushroom", "Invincibility Leaf"],
+        "answer": "Warp Whistle"
+    },
+    {
+        "question": "In Pokemon, what type is the move 'Hyper Beam'?",
+        "options": ["Normal", "Electric", "Fire", "Psychic"],
+        "answer": "Normal"
+    },
+    {
+        "question": "In Minecraft, what is the name of the material that is used to create armor?",
+        "options": ["Lapis Lazulli", "Redstone", "Iron", "Emerald"],
+        "answer": "Iron"
+    },
+    {
+        "question": "In League of Legends, which champion has the ultimate ability 'The Box'?",
+        "options": ["Thresh", "Mordekaiser", "Fiddlesticks", "LeBlanc"],
+        "answer": "Thresh"
+    },
+    {
+        "question": "What is the name of the device that allows Mario to fly in Super Mario Bros. 3?",
+        "options": ["P-Wing", "Cape Feather", "Propeller Mushroom", "Tanooki Suit"],
+        "answer": "P-Wing"
+    },
+    {
+        "question": "In Pokemon, what type is the move 'Thunderbolt'?",
+        "options": ["Electric", "Water", "Fire", "Ice"],
+        "answer": "Electric"
+    },
+    {
+        "question": "What is the name of the character who is the main antagonist in the Mario franchise?",
+        "options": ["Bowser", "Wario", "Donkey Kong", "King Boo"],
+        "answer": "Bowser"
+    },
+    {
+        "question": "In Minecraft, what is the name of the material that is used to create tools?",
+        "options": ["Redstone", "Lapis Lazulli", "Iron", "Emerald"],
+        "answer": "Iron"
+    }]
